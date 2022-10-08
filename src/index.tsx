7a4e6c42c72447de1caa18646992011f6c6d8e96
Mark <vargamarklevente@gmail.com>
2022-10-09 00:47:05 +0200

    fixed rendering according to React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
-import ReactDOM from "react-dom"
+import React from "react"
+import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
 import { AppStateProvider } from "./AppStateContext"
 
-ReactDOM.render(
-  <AppStateProvider>
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+  root.render(
+	<AppStateProvider>
     <App />
   </AppStateProvider>,
-  document.getElementById("root")
 )
